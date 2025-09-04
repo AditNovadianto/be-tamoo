95bdb8d5157c71d88d5cf3faea0c1cbceb2bf616
AditNovadianto <aditya.novadianto@gmail.com>
2025-09-04 23:13:18 +0700

    change in database

--- database/mongo.js
+++ database/mongo.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://adityanovadianto:N6xPX5rKpgePC7xz@cluster0.fhqhxcp.mongodb.net/tamoo?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
